36be6a4e9d65765090fa9510fb35c83623afa650
Christian Alamillo <calam002@ucr.edu>
2021-10-20 22:29:19 -0700

    Implemented seperation of concern with task and project modules; will implement an array to contain project objects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import {initial} from './initialize.js'
 import './styles.css'
+import {Project} from './project'
 import {Task} from './task.js'
 
+Project();
 initial();
--- src/task.js
+++ src/task.js
@@ -7,11 +7,4 @@ function Task(name, description, priority) {
 const addTask = new Task('Homework', 'Finish math problems 1-33 odd', 'HIGH');
 console.log(addTask);
 
-function Project(name) {
-    this.name = name;
-}
-
-const addProject = new Project('School');
-console.log(addProject);
-
 export {Task};
