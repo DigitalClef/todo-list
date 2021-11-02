055540afc21451a87dc4c992cb98fbe5e21db3a4
Christian Alamillo <calam002@ucr.edu>
2021-11-01 20:44:14 -0700

    Restarting project

--- src/task.js
+++
@@ -1,9 +0,0 @@
-function Task(name, description, priority) {
-    this.name = name;
-    this.description = description;
-    this.priority = priority;
-}
-
-const addTask = new Task('Homework', 'Finish math problems 1-33 odd', 'HIGH');
-
-export {Task};
